43bec6650a7c3d982fbb89b45eeebc676dd0dce8
remynz <36807398+remynz@users.noreply.github.com>
2018-04-27 04:23:36 +0200

    Update Person.java
    last change

--- model/Person.java
+++ model/Person.java
@@ -1,17 +1,14 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * FileName : Person
+ * Authors : Remy Nasserzare, Miguel Gouveia
  */
+
 package ch.heigvd.res.model;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-/**
- *
- * @author 
- */
+
 public class Person
 {
    private String firstName;
