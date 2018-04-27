365cd102de736316b35c37f6755a632504fa722c
remynz <36807398+remynz@users.noreply.github.com>
2018-04-27 04:35:44 +0200

    Update ConfigurationManagerInterface.java
    last modif of this class before pres

--- config/ConfigurationManagerInterface.java
+++ config/ConfigurationManagerInterface.java
@@ -1,28 +1,22 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * FileName : ConfigurationManagerInterface.java
+ * Authors : Remy Nasserzare, Miguel Lopes Gouveia
  */
 package ch.heigvd.res.config;
 
 import ch.heigvd.res.model.Message;
 import ch.heigvd.res.model.Person;
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
-/**
- *
- * @author 
- */
+
 public interface ConfigurationManagerInterface
 {
    public String getAddress();
    public int getPort();
    public  int getNumberOfGroup();
-
    public List<Person> getWitnessToCC();
    public List<String> getMessages();
    public List<Person> getVictimes();
