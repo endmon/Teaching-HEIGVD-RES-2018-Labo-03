fef8710ce158054bf9910d0f39f70dcb42fc6edf
remynz <36807398+remynz@users.noreply.github.com>
2018-04-27 04:22:09 +0200

    Update Group.java
    add header

--- model/Group.java
+++ model/Group.java
@@ -1,17 +1,14 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * FileName : Group
+ * Authors : Remy Nasserzare, Miguel Gouveia
  */
+
 package ch.heigvd.res.model;
 
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- *
- * @author
- */
+
 public class Group
 {
    private final List<Person> members = new ArrayList<>();
