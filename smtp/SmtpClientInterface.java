c93225440cab7dbacb6dfce45e23798b13e57b06
remynz <36807398+remynz@users.noreply.github.com>
2018-04-27 04:18:36 +0200

    Update SmtpClientInterface.java
    last modifications of smtp client interface

--- smtp/SmtpClientInterface.java
+++ smtp/SmtpClientInterface.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * FileName : SmtpClientInterface
+ * Authors : Remy Nasserzare, Miguel Gouveia
+ * 
  */
 package ch.heigvd.res.smtp;
 
@@ -9,10 +9,7 @@ import ch.heigvd.res.model.Message;
 import java.io.IOException;
 import java.net.InetAddress;
 
-/**
- *
- * @author 
- */
+
 public interface SmtpClientInterface
 {
    public void sendMessage(Message message) throws IOException;
