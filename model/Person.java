601fc1e723ce8518d872e48cba9ad8fe4a910e37
remynz <36807398+remynz@users.noreply.github.com>
2018-04-27 04:24:20 +0200

    Update Person.java
    little update

--- model/Person.java
+++ model/Person.java
@@ -31,18 +31,5 @@ public class Person
       this.mail = mail;
    }
    
-   public String getFirstName()
-   {
-      return firstName;
-   }
    
-   public String getLastName()
-   {
-      return lastName;
-   }
-   
-   public String getMail()
-   {
-      return mail;
-   }
 }
