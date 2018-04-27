5ee6dbf701f9dd3279431ae18319bcd0da64c2de
remynz <36807398+remynz@users.noreply.github.com>
2018-04-27 04:21:14 +0200

    Update SmtpClient.java
    last modifications

--- smtp/SmtpClient.java
+++ smtp/SmtpClient.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * FileName : SmtpClient
+ * Authors : Remy Nasserzare, Miguel Gouveia
  */
 package ch.heigvd.res.smtp;
 
@@ -10,10 +9,7 @@ import java.io.*;
 import java.net.Socket;
 import java.util.logging.Logger;
 
-/**
- *
- * @author endmon
- */
+
 public class SmtpClient implements SmtpClientInterface
 {
    private static final Logger LOG = Logger.getLogger(SmtpClient.class.getName());
